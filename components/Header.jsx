858206cd09da0a0c7b1314220a1298f40c1a60f0
Sharon Joseph <sharonjoseph52@yahoo.com>
2025-02-13 23:58:50 +0530

    Update

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="fixed top-0 w-full flex items-center space-x-2 p-2 bg-white shadow-md rounded-lg">
-      <img
-        src="asserts/College.jpg"
-        alt="logo"
-        className="h-20 w-20 rounded-full object-cover shadow-lg border-2 border-green-700"
-      />
-      <h1 className="text-3xl font-bold text-green-700">
-        Government Siddha Medical College - Tirunelveli
-      </h1>
+    <div className="fixed top-0 w-full flex items-center space-x-2 p-2 bg-white shadow-md rounded-lg z-50">
+      <div className="container mx-auto flex items-center space-x-2">
+        <img
+          src="asserts/College.jpg"
+          alt="logo"
+          className="h-20 w-20 rounded-full object-cover shadow-lg border-2 border-green-700"
+        />
+        <h1 className="text-3xl font-bold text-green-700">
+          Government Siddha Medical College - Tirunelveli
+        </h1>
+      </div>
     </div>
   );
 };
