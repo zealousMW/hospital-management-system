886f575eb0a7320f4f0d0d711779a91c9913629a
AravindRajalingam <rajaaravind088@gmail.com>
2025-02-28 09:19:22 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
-
-import { Plus_Jakarta_Sans } from 'next/font/google'
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/utils/utils"
-import { ThemeProvider } from 'next-themes';
-import { Toaster } from "@/components/ui/toaster"
-
+import { cn } from "@/utils/utils";
+import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/toaster";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -12,15 +10,15 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Patient Management System",
+  title: "தமிழ் சித்தன்",
   description: "A patient management system built with Supabase.",
 };
 
 const fontsans = Plus_Jakarta_Sans({
-  subsets: ['latin'],
-  weight:['200','300','400','500','600'],
-  variable: '--font-sans',
-})
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600"],
+  variable: "--font-sans",
+});
 
 export default function RootLayout({
   children,
@@ -29,19 +27,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-      <body className={cn('min-h-screen font-sans antialiased',fontsans.variable)}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          
-          >
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
+      <body
+        className={cn("min-h-screen font-sans antialiased", fontsans.variable)}
+      >
+        <ThemeProvider attribute="class" defaultTheme="light">
           {children}
           <Toaster />
-
-          </ThemeProvider>
-          <Toaster />
-         
+        </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
